08bf3da3fdca6c1f665de19b42f519ecbe9c8818
Marc Nuri <marc@marcnuri.com>
2024-05-30 14:50:41 +0200

    fix: gradle-tooling-api downloaded from Maven repository
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- src/it/system.properties.https-auth-proxy/verify.groovy
+++ src/it/system.properties.https-auth-proxy/verify.groovy
@@ -8,3 +8,4 @@ new FileWriter(new File(basedir, 'access.log')).withWriter { it << logOut }
 assert logOut.toString().contains('CONNECT services.gradle.org:443')
 def buildLog = new File(basedir, 'build.log').text
 assert buildLog.contains('Gradle 8.2.1 download complete')
+assert buildLog.contains('Gradle Tooling API 8.2.1 download complete')
