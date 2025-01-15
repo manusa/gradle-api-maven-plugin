a776d6cff4613a4280658846913e59531990776f
Marc Nuri <marc@marcnuri.com>
2025-01-15 09:17:01 +0100

    test:deps: bump gradle 8 from 8.2.1 to 8.12
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- src/it/system.properties.https-proxy/verify.groovy
+++ src/it/system.properties.https-proxy/verify.groovy
@@ -7,5 +7,5 @@ dockerStop.waitForOrKill(30_000)
 new FileWriter(new File(basedir, 'access.log')).withWriter { it << logOut }
 assert logOut.toString().contains('CONNECT services.gradle.org:443')
 def buildLog = new File(basedir, 'build.log').text
-assert buildLog.contains('Gradle 8.2.1 download complete')
-assert buildLog.contains('Gradle Tooling API 8.2.1 download complete')
+assert buildLog.contains('Gradle 8.12 download complete')
+assert buildLog.contains('Gradle Tooling API 8.12 download complete')
